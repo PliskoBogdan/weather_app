9b59d054281499554aafb2a77958c8f76a5dd304
= <=>
2023-11-18 10:41:51 +0200

    chore: use overrides jest files for config

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,13 +16,13 @@ module.exports = {
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'vue/no-parsing-error': 'off',
     'vue/multi-word-component-names': 'off',
-    'no-undef': 'off'
   },
   overrides: [
     {
       files: [
         '**/__tests__/*.{j,t}s?(x)',
-        '**/tests/unit/**/*.spec.{j,t}s?(x)'
+        '**/tests/unit/**/*.spec.{j,t}s?(x)',
+        "**/*.spec.js",
       ],
       env: {
         jest: true
