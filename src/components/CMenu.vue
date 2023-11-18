8cb1f801186065ea4fd6c12507395a6e163aa0b4
= <=>
2023-11-18 10:46:53 +0200

    feat: add CMenu component

---
+++ src/components/CMenu.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="simple-menu">
+    <div @click="toggleMenu" class="pointer">
+      <slot name="activator">Toggle Menu</slot>
+    </div>
+    <ul v-show="isOpen" @click.stop="closeMenu">
+      <slot></slot>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      isOpen: false,
+    };
+  },
+  methods: {
+    toggleMenu() {
+      this.isOpen = !this.isOpen;
+    },
+    closeMenu() {
+      this.isOpen = false;
+    },
+  },
+};
+</script>
+
+<style scoped>
+.simple-menu {
+  position: relative;
+}
+
+button {
+  cursor: pointer;
+}
+
+ul {
+  position: absolute;
+  margin: 0;
+  padding: 0;
+  list-style-type: none;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+  background-color: #c7c7c7;
+  min-height: 30px;
+}
+
+li {
+  padding: 8px;
+  cursor: pointer;
+  transition: background-color 0.3s;
+}
+
+li:hover {
+  background-color: #8d8d8d;
+}
+</style>
