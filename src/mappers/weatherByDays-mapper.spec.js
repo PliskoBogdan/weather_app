bb0f955950363812101571e59368cf06838e9715
PliskoBogdan <fanatamas64@gmail.com>
2023-11-21 03:40:31 +0200

    test: add test unit for weatherByDays mapper

---
+++ src/mappers/weatherByDays-mapper.spec.js
@@ -0,0 +1,34 @@
+import weatherByDaysMapper from './weatherByDays-mapper';
+
+describe('weatherByDaysMapper', () => {
+  test('should map weather data by day', () => {
+    const input = [
+      { dt_txt: '2023-11-21 12:00:00' },
+      { dt_txt: '2023-11-21 15:00:00' },
+      { dt_txt: '2023-11-22 09:00:00' },
+    ];
+
+    const output = weatherByDaysMapper(input);
+
+    expect(output.list).toEqual({
+      '2023-11-21': [
+        { dt_txt: '2023-11-21 12:00:00' },
+        { dt_txt: '2023-11-21 15:00:00' },
+      ],
+      '2023-11-22': [
+        { dt_txt: '2023-11-22 09:00:00' },
+      ],
+    });
+  });
+
+  test('should store current time info', () => {
+    const input = [
+      { dt_txt: '2023-11-21 12:00:00' },
+      { dt_txt: '2023-11-21 15:00:00' },
+    ];
+
+    const output = weatherByDaysMapper(input);
+
+    expect(output.currentTimeInfo).toEqual({ dt_txt: '2023-11-21 12:00:00' });
+  });
+});
