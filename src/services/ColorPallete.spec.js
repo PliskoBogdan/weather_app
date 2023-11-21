8be0038ab8bc9fc5e4153a26356db2d6a7bb08ae
PliskoBogdan <fanatamas64@gmail.com>
2023-11-21 03:40:02 +0200

    test: update colorPallete mapper

--- src/services/ColorPallete.spec.js
+++ src/services/ColorPallete.spec.js
@@ -5,24 +5,32 @@ describe('ColorPallete', () => {
     document.documentElement.style.cssText = '';
   });
 
+  it('should get pallete', () => {
+    const pallete = ColorPallete.getPallete('light');
+
+    expect(Object.keys(pallete).length).toBeGreaterThan(0);
+    expect(pallete['--text-main'].length).toBeGreaterThan(0);
+  })
+
   it('should set light theme variables', () => {
     ColorPallete.setTheme('light');
+    const pallete = ColorPallete.getPallete('light')
 
-    expect(document.documentElement.style.getPropertyValue('--sidebar-bg-color')).toBe('red');
-    expect(document.documentElement.style.getPropertyValue('--sidebar-item-hover')).toBe('green');
-    expect(document.documentElement.style.getPropertyValue('--sidebar-item-active')).toBe('#2c3e50');
-    expect(document.documentElement.style.getPropertyValue('--text-main')).toBe('#ffffff');
-    expect(document.documentElement.style.getPropertyValue('--main-soul')).toBe('blue');
+    expect(document.documentElement.style.getPropertyValue('--sidebar-bg-color')).toBe(pallete['--sidebar-bg-color']);
+    expect(document.documentElement.style.getPropertyValue('--sidebar-item-hover')).toBe(pallete['--sidebar-item-hover']);
+    expect(document.documentElement.style.getPropertyValue('--sidebar-item-active')).toBe(pallete['--sidebar-item-active']);
+    expect(document.documentElement.style.getPropertyValue('--text-main')).toBe(pallete['--text-main']);
+    expect(document.documentElement.style.getPropertyValue('--main-soul')).toBe(pallete['--main-soul']);
   });
 
   it('should set dark theme variables', () => {
     ColorPallete.setTheme('dark');
-
-    expect(document.documentElement.style.getPropertyValue('--sidebar-bg-color')).toBe('#333333');
-    expect(document.documentElement.style.getPropertyValue('--sidebar-item-hover')).toBe('#f1f1f1');
-    expect(document.documentElement.style.getPropertyValue('--sidebar-item-active')).toBe('#2c3e50');
-    expect(document.documentElement.style.getPropertyValue('--text-main')).toBe('#ffffff');
-    expect(document.documentElement.style.getPropertyValue('--main-soul')).toBe('#333333');
+    const pallete = ColorPallete.getPallete('dark')
+    expect(document.documentElement.style.getPropertyValue('--sidebar-bg-color')).toBe(pallete['--sidebar-bg-color']);
+    expect(document.documentElement.style.getPropertyValue('--sidebar-item-hover')).toBe(pallete['--sidebar-item-hover']);
+    expect(document.documentElement.style.getPropertyValue('--sidebar-item-active')).toBe(pallete['--sidebar-item-active']);
+    expect(document.documentElement.style.getPropertyValue('--text-main')).toBe(pallete['--text-main']);
+    expect(document.documentElement.style.getPropertyValue('--main-soul')).toBe(pallete['--main-soul']);
   });
 
   it('should not set invalid theme variables', () => {
